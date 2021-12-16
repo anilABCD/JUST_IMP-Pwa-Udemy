794f98cea5c7cbc128549cf1c2734e9259e58333
Anil <anilkumarpotlapally@Anils-MacBook-Pro.local>
2021-12-16 13:20:24 +0530

    caches api added

---
+++ Caches.js
@@ -0,0 +1,43 @@
+if (window.caches) {
+
+    // caches.open("test1");
+    // caches.open("test2");
+
+    // console.log(":: keys ::")
+    // caches.keys().then(console.log)
+
+    // console.log(":: has :: ")
+    // caches.has("test2").then(console.log)
+
+
+    // console.log(":: delete ::")
+    // caches.delete("test1").then(console.log)
+
+
+    caches.open("pwv1.1").then(cache => {
+
+        //cache.add("/index.html");
+
+        // cache.addAll(["/style.css", "/main.js"]);
+
+        // cache.delete("/style.css");
+
+        // cache.match("/index.html").then(res => {
+        //     return res.text()
+        // }).then(html => {
+        //     console.log(html);
+        // })
+
+
+        // cache.put("/index.html", new Response("my own html 1"));
+        // cache.match("/index.html").then(res => {
+        //     return res.text()
+        // }).then(html => {
+        //     console.log(html);
+        // })
+
+        cache.keys().then(console.log)
+
+    })
+
+}
